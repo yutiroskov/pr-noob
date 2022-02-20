08296f107bb39165ecd3cf99f5acc6d2dcaea1c9
yutiroskov <blu4stacy2020@gmail.com>
2022-02-20 14:11:18 +0530

    java file

---
+++ pyramid.java
@@ -0,0 +1,16 @@
+public class pyramid 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("pyramid printing pattern");
+        int i,j;
+        for(i=0;i<=14;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
